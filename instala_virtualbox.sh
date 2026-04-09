099b2fa23f3362ecc9392167e18d9b2ba67dc2c7
João Paulo Just Peixoto <just1982@gmail.com>
2026-04-09 19:43:19 -0300

    fix: Transfere a geração do repositório do VirtualBox para o script específico

--- instala_virtualbox.sh
+++ instala_virtualbox.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -e
 
+# Repositório
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian trixie contrib" > /etc/apt/sources.list.d/virtualbox.list
+
 # Chaves do VirtualBox
 wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg --dearmor
 
